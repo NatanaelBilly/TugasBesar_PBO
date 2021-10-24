6f685b0b3bfcd4e64348a90d37abd676ed23c253
may <maycellineselvyanti@gmail.com>
2021-10-24 22:15:40 +0700

    clean main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,5 @@ import View.HomePelanggan;
 public class Main {
     public static void main(String[] args) {
         new HomePelanggan();
-        System.out.println("aaaaa");
-        System.out.println("hii");
     }
 }
