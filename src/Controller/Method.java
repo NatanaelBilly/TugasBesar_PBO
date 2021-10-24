98afadc405ece921e32af95f53d7c0cd60bd6980
NatanaelBilly <if-20011@students.ithb.ac.id>
2021-10-24 23:50:23 +0700

    menambah controller

--- src/Controller/Method.java
+++ src/Controller/Method.java
@@ -3,11 +3,11 @@ package Controller;
 public class Method {
 
     public void lihatDaftarUser(){
-
+        System.out.println("LIST USER");
     }
 
     public void lihatFeedback(){
-
+        System.out.println("LIST FEEDBACK");
     }
 
     public void lihatSeluruhTransaksi(){
