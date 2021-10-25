085184df4a7dde6b35eb14070f606d7e9b0a8590
KaisarV <kaisar.valentino123@gmail.com>
2021-10-25 15:37:09 +0700

    add database connection

---
+++ src/Controller/Controller.java
@@ -0,0 +1,42 @@
+
+package Controller;
+
+import Database.Database;
+import Model.Role;
+import Model.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MrKaisar
+ */
+public class Controller {
+    static Database conn = new Database();
+    
+    public static ArrayList<User> getAllUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        conn.connect();
+        String query = "SELECT * FROM user";
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                User user = new User();
+                user.setId_user(rs.getInt("id_user"));
+                user.setNama_depan(rs.getString("nama_depan"));
+                user.setNama_belakang(rs.getString("nama_belakang"));
+                user.setEmail_user(rs.getString("email_user"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(Role.valueOf(rs.getString("role")));
+                user.setSaldo(rs.getInt("saldo"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (users);
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,21 @@
 package Main;
 
+import Controller.Controller;
+import Model.User;
 import View.HomePelanggan;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         new HomePelanggan();
+        
+        //Try To Print User, You can delete it
+        Controller controller = new Controller();
+        ArrayList<User> users = controller.getAllUsers();
+        
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
+        
     }
 }
