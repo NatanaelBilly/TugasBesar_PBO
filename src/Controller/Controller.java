d9da9b8124f0325e289aa2ea57e813d3dd35ac69
learngamedevelopment <adithiatirtaz@gmail.com>
2021-10-27 16:37:43 +0700

    Tambah Function Controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,6 +2,7 @@
 package Controller;
 
 import Database.Database;
+import Model.Kurir;
 import Model.Role;
 import Model.User;
 import java.sql.ResultSet;
@@ -38,5 +39,54 @@ public class Controller {
             e.printStackTrace();
         }
         return (users);
+    }
+    
+    //Kurir
+    public Kurir ambilDataKurir(int idUser)
+    {
+        Kurir dataKurir = null;
+        
+        conn.connect();
+        String query = "SELECT u.*, k.* "
+                + "FROM user u JOIN kurir k ON u.id_user = k.id_user "
+                + "AND u.id_user = " + idUser;
+        try
+        {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while (rs.next()) {
+                
+                //Cek Role
+                Role role = Role.valueOf(rs.getString("role"));
+                if(role == Role.KURIR)
+                {
+                    int id_user = rs.getInt("id_user");
+                    String nama_depan = rs.getString("nama_depan");
+                    String nama_belakang = rs.getString("nama_belakang");
+                    String email_user = rs.getString("email_user");
+                    String password = rs.getString("password");
+
+                    String NIK = rs.getString("NIK");
+                    String plat = rs.getString("plat");
+                    String jenis_kendaraan = rs.getString("jenis_kendaraan");
+                    double total_pendapatan = rs.getInt("total_pendapatan");
+                    int ketersediaan = rs.getInt("ketersediaan");
+
+                    double saldo = rs.getInt("saldo");
+                    dataKurir = new Kurir(
+                            total_pendapatan, NIK, ketersediaan, 
+                            plat, jenis_kendaraan,
+                            id_user, nama_depan, nama_belakang,
+                            email_user, password, saldo,
+                            role, null);
+                }
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return dataKurir;   
     }
 }
