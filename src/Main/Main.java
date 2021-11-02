41b69c7e1bb5002a1839a6772293257680bf5d5f
may <maycellineselvyanti@gmail.com>
2021-11-02 20:29:51 +0700

    add action listener login to landing page

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,19 +3,22 @@ package Main;
 import Controller.Controller;
 import Model.User;
 import View.HomePelanggan;
+import View.LandingPage;
+import View.Login;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        new HomePelanggan();
+        new Login();
         
         //Try To Print User, You can delete it
-        Controller controller = new Controller();
-        ArrayList<User> users = controller.getAllUsers();
-        
-        for (User user : users) {
-            System.out.println(user.toString());
-        }
+//        Controller controller = new Controller();
+//        ArrayList<User> users = controller.getAllUsers();
+//
+//        for (User user : users) {
+//            System.out.println(user.toString());
+//        }
         
     }
 }
