315cd07151ef5411bc590b2c3d30a75c57cadcf6
may <maycellineselvyanti@gmail.com>
2021-11-04 21:08:48 +0700

    add halaman 1 register untuk pilih rule

--- src/View/Login.java
+++ src/View/Login.java
@@ -7,14 +7,34 @@ import java.awt.*;
 
 public class Login {
     JFrame frame;
-    JLabel login, inputEmail, inputPassword;
+    JLabel labelEmail, labelPassword;
+    JTextField inputEmail;
+    JPasswordField inputPassword;
     JPanel panelLogin;
+    JButton buttonLogin;
 
     public Login(){
-        //label Login
-        login = new JLabel("Silahkan login dengan email dan password yang sudah didaftarkan.", SwingConstants.CENTER);
-        login.setFont(new Font("Arial", Font.BOLD, 24));
-        login.setBounds(0,50,600,80);
+        //Label Input Email
+        labelEmail = new JLabel("Email");
+        labelEmail.setFont(new Font("Arial", Font.PLAIN, 18));
+        labelEmail.setBounds(75,200,150,35);
+
+        //TextField Input Email
+        inputEmail = new JTextField();
+        inputEmail.setBounds(225,200,300,35);
+
+        //Label Input Password
+        labelPassword = new JLabel("Password");
+        labelPassword.setFont(new Font("Arial", Font.PLAIN, 18));
+        labelPassword.setBounds(75, 270,150,35);
+
+        //Input Password
+        inputPassword = new JPasswordField();
+        inputPassword.setBounds(225,270,300,35);
+
+        //Button Login
+        buttonLogin = new JButton("LOGIN");
+        buttonLogin.setBounds(350, 350, 100,45);
 
         //Panel Login
         panelLogin = new JPanel();
@@ -22,7 +42,11 @@ public class Login {
         panelLogin.setLayout(null);
         panelLogin.setBounds(0,0,600,800);
         panelLogin.setBackground(new Color(204, 180, 114));
-        panelLogin.add(login);
+        panelLogin.add(labelEmail);
+        panelLogin.add(inputEmail);
+        panelLogin.add(labelPassword);
+        panelLogin.add(inputPassword);
+        panelLogin.add(buttonLogin);
 
         //frame
         frame = new JFrame(Constant.applicationName);
