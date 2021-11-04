c61b4ea81bead5b02fe64db0547fb128b9a5de8a
may <maycellineselvyanti@gmail.com>
2021-11-04 22:52:52 +0700

    fix register kurir

--- src/View/RegisterKurir.java
+++ src/View/RegisterKurir.java
@@ -11,79 +11,83 @@ public class RegisterKurir {
     JLabel labelNamaDepan,labelNamaBelakang, labelNIK, labelPlatNomor, labelJenisKendaraan, labelEmail, labelNomorHP, labelPassword;
     JTextField inputNamaDepan, inputNamaBelakang, inputNIK, inputPlatnomor, inputJenisKendaraan, inputEmail, inputNomorHp;
     JPasswordField inputPassword;
+    JButton buttonRegister;
 
     public RegisterKurir(){
         //Label Nama Depan
         labelNamaDepan = new JLabel("Nama Depan");
         labelNamaDepan.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelNamaDepan.setBounds(65,60,150,35);
+        labelNamaDepan.setBounds(65,90,150,30);
 
         //Input Nama Depan
         inputNamaDepan = new JTextField();
-        inputNamaDepan.setBounds(230,60,250,30);
+        inputNamaDepan.setBounds(230,90,250,30);
 
         //Label Nama Belakang
         labelNamaBelakang = new JLabel("Nama Belakang");
         labelNamaBelakang.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelNamaBelakang.setBounds(65,100,150,35);
+        labelNamaBelakang.setBounds(65,140,150,30);
 
         //Input Nama Belakang
         inputNamaBelakang = new JTextField();
-        inputNamaBelakang.setBounds(230,100,250,30);
+        inputNamaBelakang.setBounds(230,140,250,30);
 
         //Label NIK
         labelNIK = new JLabel("NIK");
         labelNIK.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelNIK.setBounds(65,140,150,35);
+        labelNIK.setBounds(65,190,150,30);
 
         //Input NIK
         inputNIK = new JTextField();
-        inputNIK.setBounds(230,140,250,30);
+        inputNIK.setBounds(230,190,250,30);
 
         //Label Plat Nomor
         labelPlatNomor = new JLabel("Plat Nomor");
         labelPlatNomor.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelPlatNomor.setBounds(65,210,150,35);
+        labelPlatNomor.setBounds(65,240,150,30);
 
         //Input Plat Nomor
         inputPlatnomor = new JTextField();
-        inputPlatnomor.setBounds(230,210,250,30);
+        inputPlatnomor.setBounds(230,240,250,30);
 
         //Label Jenis Kendaraan
         labelJenisKendaraan = new JLabel("Jenis Kendaraan");
         labelJenisKendaraan.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelJenisKendaraan.setBounds(65,245,150,35);
+        labelJenisKendaraan.setBounds(65,290,150,30);
 
         inputJenisKendaraan = new JTextField();
-        inputJenisKendaraan.setBounds(230,245,250,30);
+        inputJenisKendaraan.setBounds(230,290,250,30);
 
         //Label Email
         labelEmail = new JLabel("Email");
         labelEmail.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelEmail.setBounds(65,280,150,35);
+        labelEmail.setBounds(65,330,150,30);
 
         //Input Email
         inputEmail = new JTextField();
-        inputEmail.setBounds(230,280,250,30);
+        inputEmail.setBounds(230,330,250,30);
 
         //Label Nomor HP
         labelNomorHP = new JLabel("Nomor HP");
         labelNomorHP.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelNomorHP.setBounds(65,315,150,35);
+        labelNomorHP.setBounds(65,380,150,30);
 
         //Input Nomor HP
         inputNomorHp = new JTextField();
-        inputNomorHp.setBounds(230,315,250,30);
+        inputNomorHp.setBounds(230,380,250,30);
 
         //Label Password
         labelPassword = new JLabel("Password");
         labelPassword.setFont(new Font("Arial", Font.PLAIN, 16));
-        labelPassword.setBounds(65,350,150,35);
+        labelPassword.setBounds(65,430,150,30);
 
         //Input Password
         inputPassword = new JPasswordField();
-        inputPassword.setBounds(230,350,150,35);
+        inputPassword.setBounds(230,430,250,30);
 
+        //button Register
+        buttonRegister = new JButton("Register");
+        buttonRegister.setBounds(380, 500, 100,40);
 
         //Panel
         panelRegisterKurir = new JPanel();
@@ -106,10 +110,11 @@ public class RegisterKurir {
         panelRegisterKurir.add(inputEmail);
         panelRegisterKurir.add(inputNomorHp);
         panelRegisterKurir.add(inputPassword);
+        panelRegisterKurir.add(buttonRegister);
 
 
         //Frame
-        frame = new JFrame(Constant.applicationName);
+        frame = new JFrame(Constant.applicationName+" - Register Kurir");
         frame.setSize(600,800);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
