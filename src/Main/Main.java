3a58cd41d706063ec5d5c51214c059ca8a106c74
learngamedevelopment <adithiatirtaz@gmail.com>
2021-11-05 16:28:20 +0700

    Refresh Project

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,24 +10,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        
-        
+        new HomePelanggan();
+
         //Try To Print User, You can delete it
-        
         Controller controller = new Controller();
         ArrayList<User> users = controller.getAllUsers();
-        
+
         for (User user : users) {
-            System.out.println(user.getNama_depan());
+            System.out.println(user.toString());
         }
 
-//        Controller controller = new Controller();
-//        ArrayList<User> users = controller.getAllUsers();
-//
-//        for (User user : users) {
-//            System.out.println(user.toString());
-//        }
-
-        
     }
 }
