298b9fda589a21bb745f7e022c2e494ab57371b0
learngamedevelopment <adithiatirtaz@gmail.com>
2021-11-05 16:25:39 +0700

    Refresh Project

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,20 +11,23 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         
+        
         //Try To Print User, You can delete it
+        
         Controller controller = new Controller();
-        //ArrayList<User> users = controller.getAllUsers();
-        Kurir kurirActive = controller.ambilDataKurir(2);
+        ArrayList<User> users = controller.getAllUsers();
         
-        
-        
-        
+        for (User user : users) {
+            System.out.println(user.getNama_depan());
+        }
+
+//        Controller controller = new Controller();
+//        ArrayList<User> users = controller.getAllUsers();
+//
 //        for (User user : users) {
 //            System.out.println(user.toString());
 //        }
+
         
-
-
-        new KurirMenu(kurirActive);
     }
 }
