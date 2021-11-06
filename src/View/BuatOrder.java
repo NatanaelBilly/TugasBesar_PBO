0d1f1cfef5bcd6da5a6493190ee78124dcc209da
may <maycellineselvyanti@gmail.com>
2021-11-06 17:34:48 +0700

    add GUI Buat Order

---
+++ src/View/BuatOrder.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+
+public class BuatOrder {
+    JFrame frame;
+    JPanel panelPengirim, panelPenerima, panelBarang;
+    JLabel labelPengirim, labelPenerima, labelBarang, labelNamaPenerima, labelNamaPengirim, labelAlamatPenerima, labelAlamatPengirim, labelNomorHPPenerima, labelNomorHPPengirim, labelTipeBarang, labelTotalBarang, labelBeratBarang;
+    JButton buttonOrder;
+
+    public BuatOrder(){
+
+    }
+}
