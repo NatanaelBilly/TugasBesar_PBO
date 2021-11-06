ab5d089bdce1ffbac2a421f6961e1a6d834bfbac
may <maycellineselvyanti@gmail.com>
2021-11-06 19:15:35 +0700

    add GUI Buat Order

--- src/View/BuatOrder.java
+++ src/View/BuatOrder.java
@@ -1,6 +1,10 @@
 package View;
 
+import Model.Constant;
+
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class BuatOrder {
     JFrame frame;
@@ -9,6 +13,32 @@ public class BuatOrder {
     JButton buttonOrder;
 
     public BuatOrder(){
+        //Panel Pengirim
+        panelPengirim = new JPanel();
+        panelPengirim.setBounds(50,50,240,400);
+        panelPengirim.setBorder(new Border() {
+            @Override
+            public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
 
+            }
+
+            @Override
+            public Insets getBorderInsets(Component c) {
+                return null;
+            }
+
+            @Override
+            public boolean isBorderOpaque() {
+                return false;
+            }
+        });
+
+        //Frame
+        frame = new JFrame(Constant.applicationName+" - Order");
+        frame.setSize(600,800);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.add(panelPengirim);
+        frame.setVisible(true);
     }
 }
