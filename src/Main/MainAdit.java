c746bdb7b666be81dd5fffca23df9bbb125bd97c
learngamedevelopment <adithiatirtaz@gmail.com>
2021-11-15 14:08:27 +0700

    Adding Builder Kurir

--- src/Main/MainAdit.java
+++ src/Main/MainAdit.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 import Controller.Controller;
 import Model.Kurir;
+import Model.Role;
 import View.BerandaKurir;
 import View.MenuLihatProfilKurir;
 
@@ -18,6 +14,7 @@ public class MainAdit {
     public static void main(String[] args) {
         
         Kurir kurir = new Controller().ambilDataKurir(8);
+        
         
         new BerandaKurir(kurir);
     }
