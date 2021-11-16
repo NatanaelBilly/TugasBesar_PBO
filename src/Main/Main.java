6ed5b90b8d616d52ab01b6557bedf0a0f6499215
NatanaelBilly <if-20011@students.ithb.ac.id>
2021-11-16 12:20:44 +0700

    memperbaiki GUI lihat list transaksi untuk admin

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,10 +6,11 @@ import View.*;
 
 public class Main {
     public static void main(String[] args) {
+
 //        Pelanggan pelanggan = new Pelanggan();
 //        new BerandaPelanggan(pelanggan);
 //        new MenuLogin();
 //        new MenuRegisterPilihRule();
-        new MenuRegisterPelanggan();
+//        new MenuRegisterPelanggan();
     }
 }
