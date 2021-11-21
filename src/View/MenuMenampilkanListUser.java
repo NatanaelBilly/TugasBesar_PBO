84f942d47e23ebe4aaf5d7af4a3bd4c9eb4d4369
Maycelline Selvyanti S <73425310+maycelline@users.noreply.github.com>
2021-11-22 04:02:36 +0700

    Fix GUI ADMIN menampilkan list user

--- src/View/MenuMenampilkanListUser.java
+++ src/View/MenuMenampilkanListUser.java
@@ -2,7 +2,6 @@ package View;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,44 +9,39 @@ import Model.*;
 import Controller.*;
 
 public class MenuMenampilkanListUser {
-    JFrame frame;
-    JPanel panel;
-    DefaultTableModel tableModel;
-    JTable tabelUser;
-    JScrollPane sp;
-    JButton back;
-    public MenuMenampilkanListUser(Admin admin){
-        Controller c=new Controller();
-        Method m=new Method();
-        frame = new DefaultComponentSetting().defaultFrame();
+    private JFrame frame;
+    private JPanel panel;
+    private DefaultTableModel tableModel;
+    private JTable tabelUser;
+    private JScrollPane sp;
+    private JButton back;
+    private Controller c = new Controller();
+    private DefaultComponentSetting GUI = new DefaultComponentSetting();
 
-        //panel
-        panel= new JPanel();
-        panel.setSize(600, 800);
-        panel.setBackground(new Color(201, 228, 197));
-
+    public MenuMenampilkanListUser(Admin admin) {
+        Method m = new Method();
         //init isi data
         c.getAllUsers();
-        String [][] dataUser=m.konversiListToArray(c.users);
+        String[][] dataUser = m.konversiListToArray(c.users);
 
         //init kolom
-        String column[]={"Tingkatan","Id User","Nama Depan","Nama Belakang","email"};
+        String column[] = {"Tingkatan", "Id", "Nama Depan", "Nama Belakang", "email"};
 
         //tabel data
-        tableModel=new DefaultTableModel(dataUser,column);
-        tabelUser=new JTable(tableModel);
-        tabelUser.setBackground(new Color(201, 228, 197));
-        tabelUser.setBounds(10,100,550,600);
-        JScrollPane sp=new JScrollPane(tabelUser);
-        sp.setBounds(0,100,550,600);
-        sp.setBackground(new Color(201, 228, 197));
+        tableModel = new DefaultTableModel(dataUser, column);
+        tabelUser = new JTable(tableModel);
+        tabelUser.setBackground(GUI.backGroundColor());
+        tabelUser.setBounds(10, 80, 570, 700);
+        JScrollPane sp = new JScrollPane(tabelUser);
+        sp.setBounds(10, 80, 570, 700);
+        sp.setBackground(GUI.backGroundColor());
         sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
 
         //button back
-        back = new DefaultComponentSetting().defaultButton("kembali ke main menu",10);
-        back.setBounds(400,20,200,30);
+        back = GUI.defaultButton("Kembali", 18);
+        back.setBounds(400, 20, 150, 30);
         back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -55,11 +49,18 @@ public class MenuMenampilkanListUser {
                 frame.dispose();
             }
         });
-        panel.add(back);
-        panel.add(sp);
-        //initialize panel dan frame
+
+        //panel
+        panel = new JPanel();
+        panel.setSize(600, 800);
+        panel.setBackground(GUI.backGroundColor());
         panel.setLayout(null);
         panel.setVisible(true);
+        panel.add(back);
+        panel.add(sp);
+
+        //Frame
+        frame = GUI.defaultFrame();
         frame.add(panel);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
